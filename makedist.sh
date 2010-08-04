ac2aea7db8ec94d9c1bac59c0bcf4be49e8534a2
Wolfgang Schnerring <ws@gocept.com>
2010-08-04 10:27:33 +0200

    run buildout before creating the tarball

--- makedist.sh
+++ makedist.sh
@@ -3,5 +3,7 @@
 VERSION=`sed -ne '/^release *=/s/^release *= *\(.*\)/\1/p' buildout.cfg`
 CURDIR=$PWD
 
+./bin/buildout
+
 cd parts/omelette
 tar cj --dereference --exclude .svn --exclude "*.pyc" -f $CURDIR/dist/webmailer-$VERSION.tar.bz2 gocept/imapapi gocept/restmail gocept/webmail mytum/webmail ../../CHANGES.txt ../../INSTALL.txt
