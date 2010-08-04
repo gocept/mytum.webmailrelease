a75fa56ed0d766b51ff3a03467f19b2c6ac035b3
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2010-08-04 13:15:54 +0200

    exclude pyo files from the tar ball

--- makedist.sh
+++ makedist.sh
@@ -9,6 +9,6 @@ TARBALL=$CURDIR/dist/webmailer-$VERSION.tar.bz2
 ./bin/buildout
 
 cd parts/omelette
-tar cj --dereference --exclude .svn --exclude "*.pyc" -f $TARBALL gocept/imapapi gocept/restmail gocept/webmail mytum/webmail ../../CHANGES.txt ../../INSTALL.txt
+tar cj --dereference --exclude .svn --exclude "*.pyc" --exclude "*.pyo" -f $TARBALL gocept/imapapi gocept/restmail gocept/webmail mytum/webmail ../../CHANGES.txt ../../INSTALL.txt
 
 scp $TARBALL download.gocept.com:/var/www/download.gocept.com/htdocs/webmailer
