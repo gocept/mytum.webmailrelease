b8b34003b0431a4e0b678f15d60fe6fca673a096
Wolfgang Schnerring <ws@gocept.com>
2010-08-04 10:40:35 +0200

    * added scp command to makedist script * abort on error

--- makedist.sh
+++ makedist.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -e
+
 VERSION=`sed -ne '/^release *=/s/^release *= *\(.*\)/\1/p' buildout.cfg`
 CURDIR=$PWD
+TARBALL=$CURDIR/dist/webmailer-$VERSION.tar.bz2
 
 ./bin/buildout
 
 cd parts/omelette
-tar cj --dereference --exclude .svn --exclude "*.pyc" -f $CURDIR/dist/webmailer-$VERSION.tar.bz2 gocept/imapapi gocept/restmail gocept/webmail mytum/webmail ../../CHANGES.txt ../../INSTALL.txt
+tar cj --dereference --exclude .svn --exclude "*.pyc" -f $TARBALL gocept/imapapi gocept/restmail gocept/webmail mytum/webmail ../../CHANGES.txt ../../INSTALL.txt
+
+scp $TARBALL download.gocept.com:/var/www/download.gocept.com/htdocs/webmailer
