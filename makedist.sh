05d6374c13fe543d06464de3c25d46fd382efb33
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2010-08-04 16:59:34 +0200

    update files testing.txt or final.txt with current beta or final release numbers

--- makedist.sh
+++ makedist.sh
@@ -4,11 +4,23 @@ set -e
 
 VERSION=`sed -ne '/^release *=/s/^release *= *\(.*\)/\1/p' buildout.cfg`
 CURDIR=$PWD
-TARBALL=$CURDIR/dist/webmailer-$VERSION.tar.bz2
+DISTDIR=$CURDIR/dist
+DOWNLOADDIR=download.gocept.com:/var/www/download.gocept.com/htdocs/webmailer/
+TARBALL=$DISTDIR/webmailer-$VERSION.tar.bz2
 
 ./bin/buildout
 
 cd parts/omelette
 tar cj --dereference --exclude .svn --exclude "*.pyc" --exclude "*.pyo" -f $TARBALL gocept/imapapi gocept/restmail gocept/webmail gocept/__init__.py mytum ../../CHANGES.txt ../../INSTALL.txt
 
-scp $TARBALL download.gocept.com:/var/www/download.gocept.com/htdocs/webmailer
+if [ `echo $VERSION |grep b` ]; then
+    LATEST="testing"
+else
+    LATEST="final"
+fi
+LATESTFILE=$DISTDIR/$LATEST.txt
+
+echo $VERSION >$LATESTFILE
+
+scp $TARBALL $DOWNLOADDIR
+scp $LATESTFILE $DOWNLOADDIR
